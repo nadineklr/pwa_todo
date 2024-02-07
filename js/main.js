d0de2a0e2d1b54d2dff13d16afb883b676c430a6
Nadine Keller <nadine@MacBook-Pro-von-Nadine.local>
2024-02-07 12:35:10 +0100

    service worker update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if ("serviceWorker" in navigator) {
   // on page load, the code is executed
   window.addEventListener("load", function () {
     navigator.serviceWorker
-      .register("/20_sae/pwa/pwa_todo/serviceWorker.js")
+      .register("./serviceWorker.js")
       .then(function (registration) {
         // success message
         console.log("Service Worker registered with scope:", registration.scope);
